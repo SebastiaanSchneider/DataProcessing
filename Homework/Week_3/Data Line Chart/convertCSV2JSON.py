5bc1c05d21ae85a6b7763cb59cfaf19468d539b6
Sebastiaan Schneider <sebastiaanschneider@gmail.com>
2018-11-20 15:13:06 +0100

    -

--- Homework/Week_3/Data Line Chart/convertCSV2JSON.py
+++ Homework/Week_3/Data Line Chart/convertCSV2JSON.py
@@ -19,16 +19,19 @@ def preprocessing():
             genre = row["Genre"]
             runtime = row["Runtime (min)"]
             budget = row["Budget ($mill)"]
-            gross = row["Adjusted Gross ($mill)"]
+            gross = row["Gross ($mill)"]
+            adj_gross = row["Adjusted Gross ($mill)"]
             rating = row["IMDb Rating"]
-            list = [movie, studio, release, genre, runtime, budget, gross, rating]
+            list = ([movie, studio, release, genre, runtime, budget, gross,
+                    adj_gross, rating])
             movies.append(list)
 
 
 def filter_write():
     with open("data.csv", 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(["Title", "Studio", "Release Date", "Genre", "Runtime" "Budget", "Gross", "IMDb Rating"])
+        writer.writerow(["Title", "Studio", "Release Date", "Genre", "Runtime",
+                         "Budget", "Gross", "Adjusted Gross", "IMDb Rating"])
         for movie in movies:
             writer.writerow(movie)
 
@@ -37,28 +40,11 @@ def json_write():
     for film in movies:
         jsondict[film[0]] = {"Studio": [film[1]], "Release Date": [film[2]],
                           "Genre": [film[3]], "Runtime": [film[4]],
-                          "Budget": [film[5]], "Gross": [film[6]],
-                          "IMDb Rating": [film[7]]}
+                          "Budget": [film[5]], "Adjusted Gross": [film[6]],
+                          "Gross": [film[7]], "IMDb Rating": [film[8]]}
 
     with open("data.json", 'w') as output:
         json.dump(jsondict, output)
-
-
-    # df = pd.DataFrame(pd.read_csv("data.csv")).sort_values(by=["Studio", "Release Date"])
-    # print(df)
-
-
-    # print(df["Studio"] for df["Studio"] == "WB")
-    # df.plot(df["Studio"], df["Gross"])
-    # print(sort)
-    # plt.bar(x=df["Studio"], height=df["Gross"])
-    # plt.show()
-# studio = df["Studio"]
-# print(dates)
-# gross = df["Adjusted Gross ($mill)"]
-# print(gross)
-# plt.bar(x=studio, height=gross)
-# plt.show()
 
 
 if __name__ == "__main__":
